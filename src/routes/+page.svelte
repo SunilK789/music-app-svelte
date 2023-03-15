cf7915c999d7618bc8d7e6e31f34afab22785c58
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-15 22:55:35 +0530

    added tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import FaAlgolia from 'svelte-icons/fa/FaAlgolia.svelte';
+</script>
+
+<div class="grid justify-items-center pt-5">
+	<div class="icon" >
+		<FaAlgolia />
+	</div>
+	<h1 class="font-bold text-5xl pt-5">Welcome to SvelteKit</h1>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+
+  .icon {
+        width:100px;
+        align-items: center;
+  }
+</style>
+
+
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+
+	build:{
+		rollupOptions:{
+			input:'src/m.js'
+		}
+	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
