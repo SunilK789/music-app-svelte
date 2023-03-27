cc92e9fe0568210f3596ae3aa8ae04815bdacd69
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-27 17:57:01 +0530

    styling itempage component

--- src/routes/album/[id]/+page.svelte
+++ src/routes/album/[id]/+page.svelte
@@ -1,63 +1,70 @@
 <script lang="ts">
-	import ItemPage from '$components/ItemPage.svelte';
 	import { getCopyrightSymbol } from '$helpers';
+	import { ItemPage } from '$components';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	$: console.log(data);
+
 	$: album = data.album;
+	$: console.log(album);
 </script>
 
 <ItemPage
 	title={album.name}
 	type={album.album_type}
-	color={null}
+	color={'orange'}
 	image={album.images.length > 0 ? album.images[0].url : undefined}
 >
-<p class="meta" slot="meta">
-	<span class="artists">
-		{album.artists.map(artist => artist.name).join(', ')}
-	</span>
-	<span class="date">
-		{new Date(album.release_date).getFullYear()}
-	</span>
-	<span class="tracks-count">
-		{`${album.total_tracks} Track${album.total_tracks > 1 ? 's':''}`}
-	</span>
-
-</p>
+	<p class="meta" slot="meta">
+		<span class="artists">
+			{album.artists.map((artist) => artist.name).join(', ')}
+		</span>
+		<span class="date">{new Date(album.release_date).getFullYear()}</span>
+		<span class="tracks-count"
+			>{`${album.total_tracks} Track${album.total_tracks > 1 ? 's' : ''}`}</span
+		>
+	</p>
 	<div class="tracks">
 		<ul>
 			{#each album.tracks.items as track}
-				<li>
-					{track.name}
-				</li>
+				<li>{track.name}</li>
 			{/each}
 		</ul>
-
-		<div class="credits">
-			<p class="date">
-				{new Date(album.release_date).toLocaleDateString('en', {
-					dateStyle: 'medium'
-				})}
+	</div>
+	<div class="credits">
+		<p class="date">
+			{new Date(album.release_date).toLocaleDateString('en', {
+				dateStyle: 'medium'
+			})}
+		</p>
+		{#each album.copyrights as copyright}
+			<p class="copyright">
+				{getCopyrightSymbol(copyright.type)}
+				{copyright.text}
 			</p>
-			{#each album.copyrights as copyright}
-				<p class="copyright">{getCopyrightSymbol(copyright.type)} {copyright.text}</p>
-			{/each}
-		</div>
+		{/each}
 	</div>
 </ItemPage>
 
-<!-- <h1>{album.type}</h1>
-<h5>{album.name}</h5> -->
 <style lang="scss">
+	.meta {
+		font-size: functions.toRem(13);
+		font-weight: 600;
+		span {
+			margin-right: 5px;
+			&.tracks-count {
+				font-weight: 400;
+				margin: 0 0 0 5px;
+				color: var(--light-gray);
+			}
+		}
+	}
 	.credits {
 		margin-top: 40px;
 		p {
 			color: var(--light-gray);
 			margin: 0;
 			font-size: functions.toRem(11);
-
 			&.date {
 				font-size: functions.toRem(13);
 			}
